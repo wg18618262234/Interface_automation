d82df006b31678d41451a4c6e6842bbcd28eb30d
wg18618262234 <wg18618262234@outlook.com>
2019-12-18 12:18:09 +0800

    change log config

--- unit/log.py
+++ unit/log.py
@@ -1,5 +1,7 @@
 import logging
 
 log = logging
-log.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-                level=logging.INFO)
+log.basicConfig(filename='Interface_automation.log',
+                filemode='a',
+                format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                level=logging.DEBUG)
